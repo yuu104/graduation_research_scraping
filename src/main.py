c2a1fea08941ecad1d81e997d62dec3813909a3c
yuu <yusei8171@gmail.com>
2023-06-22 16:24:54 +0900

    不要なimportを除去

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from time import sleep
 import os
